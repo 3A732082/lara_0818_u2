7574330d204b87ea1fe9eb61801ff1c701705afd
林江閤 <tkmk7q8qq@gmail.com>
2020-10-25 23:04:13 +0800

    建立共用區塊

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,9 @@
     <title>@yield('title')|MyBlog</title>
 </head>
 <body>
-<divclass="container">
+@include('layouts.partials.navigation')
+
+<div class="container">
 @yield('content')
 </div>
 </body>
