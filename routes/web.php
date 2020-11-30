c091413f3671d91dbd61e495262a146e12967402
林江閤 <tkmk7q8qq@gmail.com>
2020-11-30 19:03:13 +0800

    1130作業錯誤修正

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home',function(){
-    return view('home/index');
-});
+
 Route::get('home',[HomeController::class,'index'])->name('home.index');
 
-Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello/hello.index');
+Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
